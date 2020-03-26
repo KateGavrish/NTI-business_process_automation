b082180e51bfe34a022b9ab93c5a32fc92a093b8
gavrishekaterina <ketrin2303@yandex.ru>
2020-03-26 09:39:22 +0400

    заготовка бд ко 2 сессии

--- blanks/pyqt5/1.py
+++ blanks/pyqt5/1.py
@@ -1,1 +1,25 @@
-fname = QFileDialog.getOpenFileName(self, 'Выбрать документ', '')[0]
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class ReceiptOfComponents(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'receipt_of_components'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name_det = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("detail_category.name_det"))
+    serial_number = sqlalchemy.Column(sqlalchemy.String)
+    quantity = sqlalchemy.Column(sqlalchemy.Integer)
+    person = sqlalchemy.Column(sqlalchemy.String)
+    date = sqlalchemy.Column(sqlalchemy.Date)
+
+
+
+db_session.global_init('sql/db/drons1.sqlite')
+session = create_session()
+for user in session.query(User).filter(User.address == 'module_1'):
+    print(user)
