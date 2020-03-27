451fa370e1ea37104b1d532c4e16f7ed1e225ec8
pavelru <pahaprimorye2015@gmail.com>
2020-03-27 16:56:28 +1000

    pdf

--- make_xlsx_session_3.py
+++ make_xlsx_session_3.py
@@ -16,3 +16,33 @@ def make_xlsx_table(filename, spisok):
 
 
 # вот например: make_xlsx_table("lol", [[1, 2, 3], [4, 5, 6]])
+
+
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+pdf.output("simple_demo.pdf")
+
+
+def simple_table(spisok, spacing=1):
+    data = spisok
+    pdf = FPDF()
+    pdf.set_font("Arial", size=12)
+    pdf.add_page()
+
+    col_width = pdf.w / 4.5
+    row_height = pdf.font_size
+    for row in data:
+        for item in row:
+            pdf.cell(col_width, row_height * spacing,
+                     txt=item, border=1)
+        pdf.ln(row_height * spacing)
+
+    pdf.output('result_table.pdf')
+
+
+if __name__ == '__main__':
+    simple_table([["fdfd", "dfdf", "dfdf"], ["fddf", "fddf", "dfdffd"]])
